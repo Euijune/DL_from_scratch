752fbd00de3c8a67d9decfd5099d16ef73893208
EuiJune <choah76@khu.ac.kr>
2022-03-30 15:25:43 +0900

    Add matmul, linear regression

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,3 +1,4 @@
+from tkinter import Y
 import numpy as np
 from dezero.core import Function, as_variable
 from dezero import utils
@@ -8,11 +9,11 @@ class Reshape(Function):
 
     def forward(self, x):
         self.x_shape = x.shape
-        y = x.reshape(self.shape)   # numpy
+        y = x.reshape(self.shape)
         return y
-    
-    def backward(self, gy):     # gy is Variable instance (Dezero)
-        return reshape(gy, self.x_shape)    # dezero
+
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
 
 def reshape(x, shape):
     if x.shape == shape:
@@ -125,9 +126,40 @@ class Sum(Function):
         return y
 
     def backward(self, gy):
-        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis,
+                                        self.keepdims)
         gx = broadcast_to(gy, self.x_shape)
         return gx
 
 def sum(x, axis=None, keepdims=False):
     return Sum(axis, keepdims)(x)
+
+class MatMul(Function):
+    def forward(self, x, W):
+        y = x.dot(W)
+        return y
+
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+
+def matmul(x, W):
+    return MatMul()(x, W)
+
+class MeanSquaredError(Function):
+    def forward(self, x0, x1):
+        diff = x0 - x1
+        y = (diff ** 2).sum() / len(diff) 
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        gx0 = gy * diff * (2. / len(diff))
+        gx1 = -gx0
+        return gx0, gx1
+
+def mean_squared_error(x0, x1):
+    return MeanSquaredError()(x0, x1)
