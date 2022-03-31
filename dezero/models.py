416373e30712405ba42b9de4a803fc47f5bfc51f
EuiJune <choah76@khu.ac.kr>
2022-03-31 19:04:18 +0900

    Add model architecture (FC Layer)

---
+++ dezero/models.py
@@ -0,0 +1,28 @@
+from dezero.layers import Layer
+from dezero import utils
+import dezero.layers as L
+import dezero.functions as F
+
+class Model(Layer):
+    def plot(self, *inputs, to_file='model.png'):
+        y = self.forward(*inputs)
+        return utils.plot_dot_graph(y, verbose=True, filename=to_file)
+
+
+# Multi Layer Perceptron (Fully-Connected Layer)
+class MLP(Model):
+    def __init__(self, fc_output_sizes, activation=F.sigmoid):
+        super().__init__()
+        self.activation = activation
+        self.layers = []
+
+        for i, out_size in enumerate(fc_output_sizes):
+            layer = L.Linear(out_size)
+            setattr(self, 'l' + str(i), layer)
+            self.layers.append(layer)
+
+    def forward(self, x):
+        for l in self.layers[:-1]:
+            x = l(x)
+            x = self.activation(x)
+        return self.layers[-1](x)
