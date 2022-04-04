b7e435081e26e3527059dbf1f0a8d48bdc0f5754
EuiJune <choah76@khu.ac.kr>
2022-04-04 20:30:36 +0900

    Add dataset interface

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -5,6 +5,34 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Dataset:
+    def __init__(self, train=True, transform=None, target_transform=None):
+        self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+        if self.transform is None:
+            self.transform = lambda x: x
+        if self.target_transform is None:
+            self.target_transform = lambda x: x
+
+        self.data = None
+        self.label = None
+        self.prepare()
+
+    def __getitem__(self, index):
+        assert np.isscalar(index)   # index는 정수(스칼라)만 지원
+        if self.label is None:
+            return self.transform(self.data[index]), None
+        else:
+            return self.transform(self.data[index]),\
+                   self.target_transform(self.label[index])
+
+    def __len__(self):
+        return len(self.data)
+
+    def prepare(self):
+        pass
+
 
 # =============================================================================
 # Toy datasets
@@ -32,6 +60,11 @@ def get_spiral(train=True):
     x = x[indices]
     t = t[indices]
     return x, t
+
+
+class Spiral(Dataset):
+    def prepare(self):
+        self.data, self.label = get_spiral(self.train)
 
 
 # =============================================================================
